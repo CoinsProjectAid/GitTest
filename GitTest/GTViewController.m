9c544ddb3522e934c543437f1cdf1a250eab8cc4
DAIKI <DAIKI@10.0.1.7>
2013-10-10 15:09:12 +0900

    changed.

--- GitTest/GTViewController.m
+++ GitTest/GTViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-	NSLog(@"Hello World.");
+	NSLog(@"Hello iOS.");
 }
 
 - (void)didReceiveMemoryWarning
